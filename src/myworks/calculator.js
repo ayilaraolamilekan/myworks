cd19970ebbb22070ea104b152f46c40ad3a7e651
ayilaraolamilekan <olamilekan.adekunle@gamil.com>
2019-01-08 22:55:32 -0800

    my webpage

---
+++ src/myworks/calculator.js
@@ -0,0 +1,30 @@
+function plus(){
+    calculator.dis.value+="+";
+}
+
+function minus(){
+    calculator.dis.value+="-";
+}
+
+function multi() {
+    calculator.dis.value+="*";
+}
+
+function divide(){
+    calculator.dis.value+="/";
+}
+
+function mudulus(){
+    calculator.dis.value+="%"
+    
+}
+function dot() {
+    calculator.dis.value+=".";
+}
+
+function clearall(){
+    calculator.dis.value="";
+}
+function clearone(){
+    calculator.dis.value="~"
+}
---
+++ src/myworks/javalearn.js
@@ -0,0 +1,7 @@
+function book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+var myBook = new book("Perl", "Mohtashim");
+document.write("Book title is : " + myBook.title + "<br>");
+document.write("Book author is : " + myBook.author + "<br>");
